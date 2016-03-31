a787c30e8e45c16bdf656cca788e73b8bb23c20c
47257165p <47257165p@iespoblenou.org>
2016-03-31 18:48:56 +0200

    Creada la configuración del mapa así como la creación del nuevo tab con el que podremos subir vídeos, sonidos y fotos.

---
+++ app/src/main/java/fryendcyty/fryendcyty/NewMarker.java
@@ -0,0 +1,20 @@
+package fryendcyty.fryendcyty;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class NewMarker extends Fragment
+{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View rootView = inflater.inflate(R.layout.new_marker_tab, container, false);
+
+        return rootView;
+    }
+}
